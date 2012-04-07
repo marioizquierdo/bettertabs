65da41cadcdb8fa0fdb0cbac31954c68e524e973
Manuel Meurer <manuel.meurer@gmail.com>
2012-04-07 23:36:12 +0200

    Add code to be able to configure attach_jquery_bettertabs_inline option in an initializer

--- lib/bettertabs.rb
+++ lib/bettertabs.rb
@@ -1,2 +1,15 @@
+require 'bettertabs/configuration'
 require 'bettertabs/engine'
 require 'bettertabs/bettertabs_builder'
+
+module Bettertabs
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield configuration
+    end
+  end
+end
