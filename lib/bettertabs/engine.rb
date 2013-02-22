e6fd29e0a16b187e5bf9e3f1b9487045267c7450
Manuel Meurer <manuel.meurer@gmail.com>
2013-02-22 07:31:08 +0100

    Include BettertabsHelper explicitly

--- lib/bettertabs/engine.rb
+++ lib/bettertabs/engine.rb
@@ -2,5 +2,10 @@ require 'rails'
 
 module Bettertabs
   class Engine < Rails::Engine
+    initializer 'bettertabs.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper BettertabsHelper
+      end
+    end
   end
 end
